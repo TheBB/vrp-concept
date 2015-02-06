3a499f1f6d04baf72c47578cd250f47128330630
Eivind Fonn <evfonn@gmail.com>
2015-02-06 12:22:09 +0100

    Initial parsing work

--- src/Main.cs
+++ src/Main.cs
@@ -1,6 +1,11 @@
 using System;
-using VRP;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
+using VRP;
 using MySolver = VRP.Solver<MyInstance, MySolution, MyModification>;
 
 public class MySolution : ISolution
@@ -39,10 +44,93 @@ public class MyInstance : IInstance<MySolution,MyModification>
     }
 }
 
+public class CordeauParser
+{
+    public static void Parse(string filename)
+    {
+        IEnumerable<string> tokens =
+            File.ReadAllLines(filename)
+                .SelectMany(line => line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries));
+
+        int type = Convert.ToInt32(tokens.First());
+        tokens = tokens.Skip(1);
+
+        switch (type)
+        {
+        case 0: ParseVRP(tokens); break;
+        case 1: ParsePVRP(tokens); break;
+        case 2: ParseMDVRP(tokens); break;
+        case 3: ParseSDVRP(tokens); break;
+        case 4: ParseVRPTW(tokens); break;
+        case 5: ParsePVRPTW(tokens); break;
+        case 6: ParseMDVRPTW(tokens); break;
+        case 7: ParseSDVRPTW(tokens); break;
+        default: throw new NotSupportedException("Unrecognized instance type " + tokens.First());
+        }
+    }
+
+    private static void ParseVRP(IEnumerable<string> tokens)
+    {
+        // Global instance data
+        int nVehicles = Convert.ToInt32(tokens.ElementAt(0));
+        int nCustomers = Convert.ToInt32(tokens.ElementAt(1));
+        Debug.Assert(tokens.ElementAt(2) == "1");
+
+        // Constraints
+        int maxDuration = Convert.ToInt32(tokens.ElementAt(3));
+        int maxLoad = Convert.ToInt32(tokens.ElementAt(4));
+
+        tokens = tokens.Skip(5);
+
+        // Depot
+        ParseDepot(ref tokens);
+    }
+
+    private static void ParsePVRP(IEnumerable<string> tokens)
+    {
+        throw new NotImplementedException("PVRP");
+    }
+
+    private static void ParseMDVRP(IEnumerable<string> tokens)
+    {
+        throw new NotImplementedException("MDVRP");
+    }
+
+    private static void ParseSDVRP(IEnumerable<string> tokens)
+    {
+        throw new NotImplementedException("SDVRP");
+    }
+
+    private static void ParseVRPTW(IEnumerable<string> tokens)
+    {
+        throw new NotImplementedException("VRPTW");
+    }
+
+    private static void ParsePVRPTW(IEnumerable<string> tokens)
+    {
+        throw new NotImplementedException("PVRPTW");
+    }
+
+    private static void ParseMDVRPTW(IEnumerable<string> tokens)
+    {
+        throw new NotImplementedException("MDVRPTW");
+    }
+
+    private static void ParseSDVRPTW(IEnumerable<string> tokens)
+    {
+        throw new NotImplementedException("SDVRPTW");
+    }
+
+    private static void ParseDepot(ref IEnumerable<string> tokens)
+    {
+        tokens = tokens.Skip(7);
+    }
+}
+
 public class MainClass
 {
     public static void Main(string[] args)
     {
-        new MySolver(new MyInstance());
+        CordeauParser.Parse(args[0]);
     }
 }
